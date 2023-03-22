fc0b72ccbe0ff67476c884d33ac6c244e6b1ba1e
jiahengz <jiahengz@wicresoft.com>
2023-03-22 19:47:13 +0800

    dist

---
+++ dist/sw.js
@@ -0,0 +1,120 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-01d9f47c'], (function (workbox) { 'use strict';
+
+  self.skipWaiting();
+  workbox.clientsClaim();
+
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+  workbox.precacheAndRoute([{
+    "url": "assets/404-89ea4052.js",
+    "revision": null
+  }, {
+    "url": "assets/app-001a3ffd.js",
+    "revision": null
+  }, {
+    "url": "assets/home-6c02c5cd.js",
+    "revision": null
+  }, {
+    "url": "assets/index-b7c75e5a.css",
+    "revision": null
+  }, {
+    "url": "assets/virtual_pwa-register-48f129a3.js",
+    "revision": null
+  }, {
+    "url": "assets/workbox-window.prod.es5-dc90f814.js",
+    "revision": null
+  }, {
+    "url": "index.html",
+    "revision": "92cf914ac8c74251ab15cee0b4c07823"
+  }, {
+    "url": "favicon.svg",
+    "revision": "a795ab195c26601ea433babed25a7d0d"
+  }, {
+    "url": "safari-pinned-tab.svg",
+    "revision": "5eaf74d1c43d30e0af743b68a3f48504"
+  }, {
+    "url": "pwa-192x192.png",
+    "revision": "65f6c00ff3d88d8371df0480c1ba0272"
+  }, {
+    "url": "pwa-512x512.png",
+    "revision": "20a2db7d5040eb315e6acf49c6983de9"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "3b59667a352cdfd59d6bd12346d9f27e"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+
+}));
